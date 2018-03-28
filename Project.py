cfe19ec7716163aa08cbf1f0d4c7df27b9f91466
brensw <sweeney.b@me.com>
2018-03-28 13:43:04 +0100

    added scatter matrix option and made improvements to other options

--- Project.py
+++ Project.py
@@ -7,6 +7,7 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
 
 
 
@@ -15,6 +16,7 @@ from sklearn.ensemble import RandomForestClassifier
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'species']
 dataset = pandas.read_csv(url, names=names)
+df = pandas.DataFrame(dataset)
 
 
 
@@ -22,16 +24,17 @@ print ("Please enter an option")
 print ("To display dataset enter: 1")
 print ("To see a summary of the data enter: 2 ")
 print ("To create a scatter plot of petal length vs sepal length enter: 3")
-print ('To see relative importance of each varible enter: 4')
-print ("To exit enter: 5")
+print("To create a scatter matirx for all varibles enter: 4")
+print ('To see relative importance of each varible enter: 5')
+print ("To exit enter: 6")
 
 n_in= int(input("Please enter the number of your choice:") )
-if n_in >= 6:
+if n_in >= 7:
     print ("Please enter a valid number")
-elif n_in == 5: # exit application
+elif n_in == 6: # exit application
     raise SystemExit()
 elif n_in == 1: #display the dataset :https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
-    print(dataset.head(150))
+    print(df)
 elif n_in == 2: #show basic analysis of dataset: https://machinelearningmastery.com/machine-learning-in-python-step-by-step/
     print(dataset.describe())
 elif n_in == 3: #create colour scatter plot for each species:  https://www.kaggle.com/camontanezp/learning-python-data-analysis-with-iris
@@ -42,7 +45,11 @@ elif n_in == 3: #create colour scatter plot for each species:  https://www.kaggl
     dataset[dataset.species == "Iris-versicolor"].plot(kind="scatter", x="sepal-length", y="petal-length", 
     color="blue", label="Iris-versicolor", ax=ax)
     plt.show()
+
 elif n_in == 4:
+    sns.pairplot(df, hue='species',palette="husl",markers=["o", "s", "D"]) #https://seaborn.pydata.org/generated/seaborn.pairplot.html
+    plt.show()
+elif n_in == 5:
     X = dataset.iloc[:,0:4]
     Y = dataset.iloc[:,-1]
     names = dataset.columns.values
@@ -51,11 +58,12 @@ elif n_in == 4:
     importance = rfc.feature_importances_
     sorted_importances = np.argsort(importance)
     padding = np.arange(len(names)-1) + 0.5
-    plt.barh(padding, importance[sorted_importances], align='center')
+    plt.barh(padding,importance[sorted_importances], color='g', align='center')
     plt.yticks(padding, names[sorted_importances])
     plt.xlabel("Relative Importance")
     plt.title("Variable Importance")
     plt.show()
+
 
 
         
